e03f716cc8d226167d3c8a230101de395c4a83a9
Santosh Potu <santoshreddy.potu@gmail.com>
2022-12-18 11:55:50 +0530

    First Commit

---
+++ console1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = System.ConsoleColor.Green;
+Console.ForegroundColor = System.ConsoleColor.Blue;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Second Line");
+Console.Write("\n Same Line");
+Console.WriteLine(" Same Line2");
+Console.Title = "Hello";
+Console.Beep();
+
+
+
+
+
