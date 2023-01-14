9408a8f0e5d2e90474d6732cb645d91954a29a59
Santosh Potu <santosh-potu@users.noreply.github.com>
2023-01-14 22:27:25 +0530

    git add

---
+++ EFGetStarted/Model.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext:DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+}
+
+
+public class Blog
+{
+    public int BlogId { get; set; } 
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new List<Post>();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Content { get; set; }
+    public string Title { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
